769e26a597c5e9e0dda793751fbcd981e064d668
Alevtina-Babinskaya <letters4alka@gmail.com>
2026-01-15 00:48:31 -0800

    picture size, repeted paragraph, picture location

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,14 +5,13 @@ function Portfolio() {
   return (
     <div >
       <h1>Portfolio</h1>
-      <h1>{location.hash}</h1>
+      
       <div className="portfolio_section" id="case1">
           <p className='header3Link'><img className="logo"/><a href='https://mediaboost.com/'>MediaBoost</a></p> 
-          <img className="cover" src='/img/dashboard.png' width='75%' alt="Personal Information" />
+          <div><img className="cover" src='/img/mediaboost/dashboard.png' width='30%' alt="Media Boost Personal Information" /></div>
           <p className="description">Media Boost is an advertising service that allows small businesses create and run ads in social media. My task here was to design profile pages for MediaBoost customers.</p>
           {location.hash == "#case1" ? 
           <div>
-            <p>Media Boost is an advertising service that allows small businesses create and run ads in social media. My task here was to design profile pages for MediaBoost customers.</p>
           <p>I started with business interviews to define the main functionality and approximate user scenarios.</p>
           <p>Running ad campaigns on social media seems a hard task for most individual businesses that don't have expertise in digital marketing. The whole idea of Media Boost is to offer one-click solutions for digital advertising, that would not involve much of the customer's attention.</p>
           <p>This is the reason why the Media Boost profile doesn't contain any diagrams and statistics about social media advertising.</p>
@@ -23,7 +22,7 @@ function Portfolio() {
       </div>
       <div className="portfolio_section" id="case2">
           <p className='header3Link'><img className="logo"/><a href='https://mediaboost.com/'>Time Timer</a></p> 
-          <img className="cover" src='/img/dashboard.png' width='100%' />
+          <img className="cover" src='/img/mediaboost/dashboard.png' width='100%' />
           <p className="description">Media Boost is an advertising service that allows small businesses create and run ads in social media. My task here was to design profile pages for MediaBoost customers.</p>
           <p>Media Boost is an advertising service that allows small businesses create and run ads in social media. My task here was to design profile pages for MediaBoost customers.</p>
           <p>I started with business interviews to define the main functionality and approximate user scenarios.</p>
